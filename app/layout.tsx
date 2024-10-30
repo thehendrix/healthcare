db77d6eb429e7bd9c25172538279fabd6a951e07
Marc Hendrikse <Marc.Hendrikse@virginorbit.com>
2024-10-29 21:58:43 -0700

    feat: UI & Lint Config
    * Added shadcn UI
    * Fixed ESLint config
    * Fixed styling

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,19 @@
-import type { Metadata } from "next";
-import { Plus_Jakarta_Sans } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next';
+import { Plus_Jakarta_Sans } from 'next/font/google';
+import { ThemeProvider } from '@/components/ui/theme-provider';
+import './globals.css';
 
-import { cn } from "@/lib/utils";
+import { cn } from '@/lib/utils';
 
 const fontSans = Plus_Jakarta_Sans({
-  subsets: ["latin"],
+  subsets: ['latin'],
   weight: ['300', '400', '500', '600', '700'],
   variable: '--font-sans'
 });
 
 export const metadata: Metadata = {
-  title: "CarePulse",
-  description: "A healthcare managemnet system",
+  title: 'CarePulse',
+  description: 'A healthcare management system',
 };
 
 export default function RootLayout({
@@ -22,7 +23,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={cn('min-h-screen bg-dark-300 font-sans antialiased', fontSans.variable) }>{children}</body>
+      <body className={cn('min-h-screen bg-dark-300 font-sans antialiased', fontSans.variable) }>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-import Image from "next/image";
+import { Button } from '@/components/ui/button';
 
 export default function Home() {
   return (
     <div>
       <h1 className="text-white text-3xl underline">Home</h1>
+      <Button>Click Me!</Button>
     </div>
   );
 }
